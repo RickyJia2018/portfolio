66a719faace95174241e14345b150900eebaf00a
ricky.jia <ricky.jia2018@gmail.com>
2024-01-16 15:46:13 -0500

    update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,22 +1,81 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+import Link from 'next/link';
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
         <div className="flex items-center justify-center">
             <div className='relative'>
-                <Image src="https://avatars.githubusercontent.com/u/38337250?v=4" alt="Ricky Jia" 
+              <motion.div
+                initial = {{ opacity:0, scale:0 }}
+                animate = {{ opacity:1, scale: 1.1}}
+                transition={{
+                  type: "tween",
+                  duration: 0.2
+                }}
+              >
+              <Image src="https://avatars.githubusercontent.com/u/38337250?v=4" alt="Ricky Jia" 
                 width="192"
                 height="192"
                 quality="95"
                 priority={true}
                 className='h-24 w-24 rounded-full object-center border-[0.35rem] border-white shadow-xl'/>
 
-                <span className='text-4xl'>
-                    👋👋
-                </span>
+              </motion.div>
+         
+                <motion.span className='absolute bottom-0 right-0 text-4xl'
+                initial={{ opacity:0, scale:0}}
+                animate={{ opacity:1, scale:1}}
+                transition={{
+                  type: "spring",
+                  stiffness: 1200,
+                  delay: 0.1,
+                  duration: 0.7
+                }}>
+                    👋
+                </motion.span>
             </div>
         </div>
+        <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Ricky.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">4 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>
+        <span> and </span>
+        <span className="underline">Golang (gRPC)</span>
+        .
+      </motion.h1>
+      <motion.div className='flex flex-col sm:flex-row gap-2 px-4 text-lg font-medium'
+        initial={{ opacity:0, y:100 }}
+        animate={{ opacity:1, y:0 }}
+        transition={{
+          delay:0.1,
+        }}
+      >
+        <Link href="#contact"
+        className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:gb-gray-950 active:scale-105 transition">
+          Contact me here <BsArrowRight className='opacity-70'/> {" "}
+        </Link>
+        <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">
+          Download Resume <HiDownload/>{" "}
+        </a>
+        <a className="bg-white p-4 text-gray-700 flex rounded-full items-center gap-2">
+          <BsLinkedin />
+        </a>       
+        <a className="bg-white p-4 text-gray-700 flex rounded-full items-center gap-2">
+          <BsGithub />
+        </a>
+      </motion.div>
     </section>
   )
 }
