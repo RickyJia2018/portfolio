c42f760ccf27d28d2dade1d9e54596c7c85f6504
NingqianJia <ricky.jia2018@gmail.com>
2024-01-16 21:17:06 -0500

    updated header and context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from "@/components/header"
+import ActiveSectionContextProvider from "@/context/active-section-context";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,8 +20,12 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 h-[1000rem]`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] -z-10'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] -z-10'></div>
-        <Header/>
-        {children}
+        <ActiveSectionContextProvider>
+
+          <Header/>
+          {children}
+        </ActiveSectionContextProvider>
+
         </body>
     </html>
   )
