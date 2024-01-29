ebe12f88c64348d0f988c576fa574a94c115e842
ricky.jia <ricky.jia2018@gmail.com>
2024-01-29 14:59:53 -0500

    update about

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,16 @@ export default function About() {
     <motion.section id='about' ref={ref} className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'initial={{ opacity:0, y:100}} animate={{ opacity:1, y:0 }} transition={{delay:0.175}}>
         <SectionHeading >About Me</SectionHeading>
         <p>
-123
+        In high school, I was fascinated by World of Warcraft, sparking a desire to become a creator in the realm of binary. This led me to choose computer science as my major in university. 
+     
+        </p>
+        <p>   I really <span className='underline font-bold'>love</span> writing code to create things. It has become <span className='italic'>an integral part of my life.{" "}</span>
+        My core stack is <span className='font-medium'>React, Golang(Gin & gRPC) and Postgres.</span> I am also experienced with Node.js, Flutter, MongoDB and Laravel.
+        To keep my skills sharp, I am always eager to learn new technologies. 
+        </p>
+        <p className='mt-3 mb-3'>I am currently looking for a <span className='font-medium' >full-time position as a software developer.</span></p>
+        <p>
+          When I'm not coding, I enjoy playing video games, fishing, hiking and skiing(CSIA L2). Welcome ski with me.
         </p>
     </motion.section>
   )
